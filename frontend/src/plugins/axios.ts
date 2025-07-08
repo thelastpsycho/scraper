8216b4055fe08c3bf06bfed21907003cf69d5da4
Andi Krisnatha <andikrisnatha@Andis-MacBook-Air.local>
2025-07-08 17:04:18 +0800

    initial commit

--- frontend/src/plugins/axios.ts
+++ frontend/src/plugins/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://127.0.0.1:5000',
+  baseURL: 'http://10.201.59.16:5000',
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
